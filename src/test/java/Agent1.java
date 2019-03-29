209cb0a558addc8a9b155cde90bb4dbf5028c30b
Nikosz <nikoszp@gmail.com>
2019-03-29 10:54:25 +0100

    change agent to non-random

--- src/test/java/Agent1.java
+++ src/test/java/Agent1.java
@@ -1,5 +1,4 @@
 import model.Action;
-import model.E;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,26 +14,23 @@ public class Agent1 {
             int currentUnitId = scanner.nextInt();
             long startingTime = System.currentTimeMillis();
             scanner.nextLine();
-            System.err.println("elapsedTime 1: " + (System.currentTimeMillis() - startingTime));
 
             int numberOfActions = scanner.nextInt();
             scanner.nextLine();
-            System.err.println("elapsedTime 2: " + (System.currentTimeMillis() - startingTime));
 
             List<Action> validActions = new ArrayList<>();
             for (int i = 0; i < numberOfActions; i++) {
                 String[] actionString = scanner.nextLine().split(" ");
                 validActions.add(new Action(actionString[0], actionString[1]));
             }
-            System.err.println("elapsedTime 3: " + (System.currentTimeMillis() - startingTime));
 
-            int randomMoveIndex = E.random.nextInt(numberOfActions);
-            Action chosenAction = validActions.get(randomMoveIndex);
-            System.err.println("chosenAction: " + chosenAction);
-            System.err.println("elapsedTime 4: " + (System.currentTimeMillis() - startingTime));
+//            int randomMoveIndex = E.random.nextInt(numberOfActions);
+//            Action chosenAction = validActions.get(randomMoveIndex);
 //            Action chosenAction = validActions.size() > 1 ? validActions.get(1) : validActions.get(0);
-//            Action chosenAction = validActions.get(counter++ % (validActions.size()));
+            Action chosenAction = validActions.get(counter++ % (validActions.size()));
             System.out.println(chosenAction);
+            System.err.println("chosenAction: " + chosenAction);
+            System.err.println("elapsedTime: " + (System.currentTimeMillis() - startingTime));
         }
     }
 }
